2c2ab4ac25b260d80fc9a3ca34eb096db3e0dc08
fermincuevasimje-eng <fermin.cuevas.imje@gmail.com>
2026-05-27 14:48:12 -0600

    Create data_manager.py

---
+++ data_manager.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import json
+import config
+
+# --- Funciones para Inventario ---
+def cargar_inventario():
+    try:
+        return pd.read_csv(config.ARCHIVO_INVENTARIO)
+    except:
+        return None
+
+def guardar_inventario(df):
+    df.to_csv(config.ARCHIVO_INVENTARIO, index=False)
+
+# --- Funciones para Vales ---
+def cargar_vales():
+    try:
+        with open(config.ARCHIVO_VALES, 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except:
+        return []
+
+def guardar_vales(lista_vales):
+    with open(config.ARCHIVO_VALES, 'w', encoding='utf-8') as f:
+        json.dump(lista_vales, f, ensure_ascii=False, indent=4)
