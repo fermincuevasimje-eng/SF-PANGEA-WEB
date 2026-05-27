6986471fdc0873e7521eb2711405c4f93610ae06
fermincuevasimje-eng <fermin.cuevas.imje@gmail.com>
2026-05-27 14:45:05 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,14 @@
+# config.py
 
+# Credenciales de Seguridad
+PIN_MAESTRO = "1827"
+PIN_ALMACEN = "DAP-2026"
+
+# Rutas de Archivos
+ARCHIVO_INVENTARIO = 'inventario_dap_guardado.csv'
+ARCHIVO_VALES = 'vales_historial_guardado.json'
+ARCHIVO_TERRITORIAL = 'DELEUTB2.csv'
+
+# Constantes de Operación
+BASE_COORDS = (19.291395219739588, -99.63555838631413)
+LEYENDA_OFICIAL = "Este material es propiedad del Ayuntamiento de Toluca y se genera en la Dirección de Alumbrado Público"
