fccff18f80bb69916b0bb75c799e841b5c28c2a6
fermincuevasimje-eng <fermin.cuevas.imje@gmail.com>
2026-07-03 20:46:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1761,8 +1761,8 @@ else:
         # ==================================================================================
         # 📝 AQUÍ TERMINA OFICIOS (TAB_O) Y EMPIEZA INDEPENDIENTE LA NUEVA PESTAÑA (TAB_J)
         # ==================================================================================
-with tab_j:
-                    st.subheader("📝 Control de Justificaciones e Incidencias de Personal")
+        with tab_j:
+            st.subheader("📝 Control de Justificaciones e Incidencias de Personal")
                     
                     # --- SINCRO-BÓVEDA LOCAL (EXCLUSIVA PARA JUSTIFICACIONES) ---
                     if "db_justificaciones" not in st.session_state:
