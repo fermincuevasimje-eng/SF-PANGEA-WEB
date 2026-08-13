5edefb579975a8a75531f8551ae0cc15bad29fa8
fermincuevasimje-eng <fermin.cuevas.imje@gmail.com>
2026-08-13 11:18:09 -0600

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 # config.py
 
-# Credenciales de Seguridad
+# Credenciales de Seguridad Maestro
+USUARIO_MAESTRO = "SF"
+PASS_MAESTRO = "2938"
+
 PIN_MAESTRO = "1827"
 PIN_ALMACEN = "DAP-2026"
 
@@ -8,6 +11,13 @@ PIN_ALMACEN = "DAP-2026"
 ARCHIVO_INVENTARIO = 'inventario_dap_guardado.csv'
 ARCHIVO_VALES = 'vales_historial_guardado.json'
 ARCHIVO_TERRITORIAL = 'DELEUTB2.csv'
+
+# Catálogo Oficial Consolidado (10 Base + Sub-selecciones)
+USUARIOS_OFICIALES = [
+    "SF", "Director", "Jefe Mant", "Jefe Infra", 
+    "GuaDAP", "Almacen1", "Almacen2", "Ditto", 
+    "Especial", "Brigada"
+]
 
 # Constantes de Operación
 BASE_COORDS = (19.291395219739588, -99.63555838631413)
