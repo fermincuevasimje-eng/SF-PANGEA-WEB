4e70a7c48cfe474726418cf0cd7e651be556d822
fermincuevasimje-eng <fermin.cuevas.imje@gmail.com>
2026-09-08 16:51:39 -0600

    Update app.py

--- app.py
+++ app.py
@@ -2054,8 +2054,8 @@ else:
                             tmp_hdr_path = tmp_hdr.name
                         try:
                             # Ajuste: Encabezado pegado arriba (Y=6mm) y más ancho (W=190mm)
-                            pdf.image(tmp_hdr_path, x=13, y=6, w=190)
-                            pdf.set_y(32)
+                            pdf.image(tmp_hdr_path, x=13, y=4, w=190)
+                            pdf.set_y(38)
                         finally:
                             try: os.unlink(tmp_hdr_path)
                             except: pass
@@ -2069,7 +2069,7 @@ else:
                             tmp_ftr_path = tmp_ftr.name
                         try:
                             # Ajuste: Pie pegado al borde inferior (Y=page_h-18mm) y más ancho (W=190mm)
-                            pdf.image(tmp_ftr_path, x=13, y=page_h - 18, w=190)
+                            pdf.image(tmp_ftr_path, x=13, y=page_h - 26, w=190)
                         finally:
                             try: os.unlink(tmp_ftr_path)
                             except: pass
@@ -2706,7 +2706,7 @@ else:
                         tmp_j_hdr_path = tmp_j_hdr.name
                     try:
                         # Ajuste: Encabezado pegado arriba (Y=6mm) y más ancho (W=190mm)
-                        pdf_j.image(tmp_j_hdr_path, x=13, y=6, w=190)
+                        pdf_j.image(tmp_j_hdr_path, x=13, y=4, w=190)
                     finally:
                         try: os.unlink(tmp_j_hdr_path)
                         except: pass
@@ -2721,7 +2721,7 @@ else:
                         tmp_j_ftr_path = tmp_j_ftr.name
                     try:
                         # Ajuste: Pie pegado al borde inferior (Y=page_h_j-18mm) y más ancho (W=190mm)
-                        pdf_j.image(tmp_j_ftr_path, x=13, y=page_h_j - 18, w=190)
+                        pdf_j.image(tmp_j_ftr_path, x=13, y=page_h_j - 26, w=190)
                     finally:
                         try: os.unlink(tmp_j_ftr_path)
                         except: pass
